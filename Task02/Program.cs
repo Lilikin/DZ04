dd49d3c4d44b69f8f7e181ffca6a93e168e159da
Olesya K <olesjakraineva@gmail.com>
2023-12-01 18:37:10 +0300

    Added tasks

---
+++ Task02/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2: 
+// Задайте массив заполненный случайными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int size = 10;
+int[] array = new int[size];
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+}
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Массив: [{string.Join("; ", array)}]");
+Console.WriteLine($"Чётных чисел в массиве: {count}");
---
+++ Task03/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 3: 
+// Напишите программу, которая перевернёт одномерный массив 
+// (первый элемент станет последним, второй – предпоследним и т.д.)
+
+int size = 10;
+int[] array = new int[size];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 101);
+}
+Console.WriteLine($"Массив до изменения: [{string.Join("; ", array)}]");
+Array.Reverse(array);
+Console.WriteLine($"Массив после изменения: [{string.Join("; ", array)}]");
